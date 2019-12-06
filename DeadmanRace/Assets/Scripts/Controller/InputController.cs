1af658effa3426cef40ac682b3730525fec21e4c
Ilya <50113149+Sukhov63@users.noreply.github.com>
2019-12-06 10:59:29 +0400

    movement

---
+++ DeadmanRace/Assets/Scripts/Controller/InputController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+
+namespace DeadmanRace
+{
+    public sealed class InputController : IExecuteController
+    {
+        #region Fields
+        
+        private readonly GameContext _context;
+
+        #endregion
+
+
+        #region ClassLifeCycles
+        
+        public InputController(GameContext context, Services services)
+        {
+            _context = context;
+        }
+
+        #endregion
+
+
+        #region IExecuteController
+        
+        public void Execute()
+        {
+            var h = Input.GetAxis("Horizontal");
+            var v = Input.GetAxis("Vertical");
+            Debug.Log("OnWork");
+            Debug.Log(h);
+            Debug.Log(v);
+            _context.MyCharacter.Walk(h, v);
+
+        }
+
+        #endregion
+    }
+}
