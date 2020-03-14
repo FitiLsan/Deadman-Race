7210775ed5c74cbfe2b52530734023ec4946d54d
Maxyber <ghostpaladin@mail.ru>
2020-03-14 08:13:53 +0300

    Замечания исправлены

--- DeadmanRace/Assets/Scripts/Model/UnitMotor2D.cs
+++ DeadmanRace/Assets/Scripts/Model/UnitMotor2D.cs
@@ -1,7 +1,8 @@
 ﻿using DeadmanRace;
+using System;
 using UnityEngine;
 
-public sealed class UnitMotor : IWalk
+public sealed class UnitMotor2D : IWalk
 {
     private Transform _instance;
 
@@ -16,14 +17,13 @@ public sealed class UnitMotor : IWalk
 
     #endregion
 
-    private Rigidbody _rigidBody;
+    private Rigidbody2D _rigidBody;
 
 
-    public UnitMotor(Transform instance, PlayerData playerData)
+    public UnitMotor2D(Transform instance, PlayerData playerData)
     {
         _instance = instance;
-        _rigidBody = _instance.gameObject.GetComponent<Rigidbody>();
-        _rigidBody.constraints = RigidbodyConstraints.FreezeRotation;
+        _rigidBody = _instance.gameObject.GetComponent<Rigidbody2D>();
         _maxSpeed = playerData.MaxSpeed;
         _rotationSpeed = playerData.RotationSpeed;
         _accelerationForce = playerData.AccelerationForce;
@@ -33,31 +33,36 @@ public sealed class UnitMotor : IWalk
 
     public Transform Transform { get; set; }
 
-    public void Walk(float horizontalAxis, float verticalAxis)
+    public void Walk(float hAxis, float vAxis)
     {
-        CharacterRotation(horizontalAxis);
-        CharacterMove(horizontalAxis, verticalAxis);
+        CharacterRotation(hAxis);
+        CharacterMove(hAxis, vAxis);
     }
 
     private void CharacterMove(float horizontalAxis, float verticalAxis)
     {
-        if (verticalAxis > 0.05f)
+        if (verticalAxis > 0.1f)
             _curSpeed += _accelerationForce * verticalAxis;
         else
+        {
+            CustomDebug.Log($"Iteration1: CurSpeed = {_curSpeed}");
             _curSpeed += -1 * (_brakeForce * 0.5f / _clatch) + (_brakeForce / _clatch) * verticalAxis;
-        //CustomDebug.Log($"Iteration1: CurSpeed = {_curSpeed}");
+        CustomDebug.Log($"Iteration2: vAxis = {verticalAxis}, CurSpeed = {_curSpeed}, Brake speed = {-1 * (_brakeForce * 0.5f / _clatch) + (_brakeForce / _clatch) * verticalAxis}");
+            // CustomDebug.Log($"Iteration2: CurSpeed = {_curSpeed}");
+        }
         if (_curSpeed > _maxSpeed * 0.98f) _curSpeed = _maxSpeed;
-        if (_curSpeed < 0.05f * _maxSpeed) _curSpeed = 0.0f;
+        else if (_curSpeed < -1 * _maxSpeed * 0.98f) _curSpeed = -1 * _maxSpeed;
+        else if (Math.Abs(_curSpeed) < 0.05f * _maxSpeed) _curSpeed = 0.0f;
         // CustomDebug.Log($"Iteration2: CurSpeed = {_curSpeed}");
-        _rigidBody.AddForce(_instance.transform.forward * _curSpeed);
+        _rigidBody.AddForce(_instance.transform.up * _curSpeed);
     }
     private void CharacterRotation(float horizontalAxis)
     {
         if (horizontalAxis != 0.0f)
         {
             if (_curSpeed < 0.1f * _maxSpeed)
-                _rigidBody.AddForce(_instance.transform.forward * _rigidBody.mass * 6.0f);
-            _instance.rotation *= Quaternion.Euler(0.0f, horizontalAxis * _rotationSpeed, 0.0f);
+                _rigidBody.AddForce(_instance.transform.up * _rigidBody.mass * 6.0f);
+            _instance.rotation *= Quaternion.Euler(0.0f, 0.0f, horizontalAxis * _rotationSpeed);
             _instance.gameObject.transform.rotation = _instance.rotation;
         }
     }
